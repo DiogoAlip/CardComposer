1bcdc72d5a7e410a4c2bf449cf32bdefce6d9e80
DiogoAlip <diogoalipazaga@gmail.com>
2025-12-30 01:02:12 -0500

    feat: CardDeck added

---
+++ app/components/custom/DeckCard.tsx
@@ -0,0 +1,28 @@
+import { Card } from "../ui/card";
+
+export interface CardProps {
+    rank: string;
+    suit: string;
+    isFaceUp: boolean;
+    isIt: boolean;
+    color: string;
+}
+
+export function DeckCard({rank, suit, isFaceUp, isIt, color}: CardProps) {
+    if (isIt && !isFaceUp) {
+        return (
+            <Card className="w-[80px] h-[110px] flex flex-col bg-gradient-to-br from-primary to-accent">
+            </Card>
+        )
+    }
+    return (
+        <Card
+            className={`w-[80px] h-[110px] flex flex-col ${ isIt ? "bg-white/95" : "bg-transparent border-0"} ${color === "black" ? "text-black/85" : "text-accent"}`}
+        >
+            {isIt && <div className="flex flex-col items-center h-full justify-around">
+                <p className="text-2xl">{rank}</p>
+                <p className="text-2xl">{suit}</p>
+            </div>}
+        </Card>
+    )
+}
--- app/components/custom/DeckTemplate.tsx
+++ app/components/custom/DeckTemplate.tsx
@@ -1,5 +1,4 @@
-import { Car } from "lucide-react";
-import { Card } from "../ui/card";
+import { DeckCard } from "../custom/DeckCard";
 
 export default function DeckTemplate() {
 
@@ -8,31 +7,54 @@ export default function DeckTemplate() {
             suit: "♠",
             rank: "A",
             color: "black",
-            isFaceUp: false
+            isFaceUp: true,
+            isIt: true
+        },
+        {
+            suit: "♥",
+            rank: "3",
+            color: "red",
+            isFaceUp: false,
+            isIt: true
+        },
+        {
+            suit: "♦",
+            rank: "8",
+            color: "red",
+            isFaceUp: false,
+            isIt: false
+        },
+        {
+            suit: "♦",
+            rank: "4",
+            color: "red",
+            isFaceUp: true,
+            isIt: true
+        },
+        {
+            suit: "♦",
+            rank: "4",
+            color: "red",
+            isFaceUp: false,
+            isIt: true
         }
     ]
 
   return (
     <div className="flex flex-col h-[100vh] w-full">
       <div className="bg-black/95 h-[50%] w-full flex flex-row justify-center items-center">
+      <div className="grid grid-cols-4 w-fit gap-2">
         {CardsFromPlayer1Exmaple.map((card, index) => (
-            <Card key={index} className="w-[100px] h-[140px] flex flex-col">
-                <div className="flex flex-col items-center h-full justify-around">
-                    <p className="text-4xl">{card.rank}</p>
-                    <p className="text-4xl">{card.suit}</p>
-                </div>
-            </Card>
+            <DeckCard key={index} {...card} />
         ))}
       </div>
+      </div>
       <div className="bg-white/5 h-[50%] w-full flex flex-row justify-center items-center">
+        <div className="grid grid-cols-4 w-fit gap-2">
         {CardsFromPlayer1Exmaple.map((card, index) => (
-            <Card key={index} className="w-[100px] h-[140px] flex flex-col">
-                <div className="flex flex-col items-center h-full justify-around">
-                    <p className="text-4xl">{card.rank}</p>
-                    <p className="text-4xl">{card.suit}</p>
-                </div>
-            </Card>
+            <DeckCard key={index} {...card} />
         ))}
+        </div>
       </div>
     </div>
   );
