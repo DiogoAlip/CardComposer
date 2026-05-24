5b35b7a0ab9e84441ce08698f52fe237f0028aae
DiogoAlip <diogoalipazaga@gmail.com>
2026-05-24 08:46:54 -0500

    fix: multiplayer somming soon

--- app/routes/gamemode.route.tsx
+++ app/routes/gamemode.route.tsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
 import { Navbar } from "~/ui/NavBar.ui";
 import { Link, useNavigate } from "react-router";
-import { Bot, Mail, DoorOpen, Clipboard as ClipboardIcon, ArrowRight } from "lucide-react";
+import {
+  Bot,
+  Mail,
+  DoorOpen,
+  Clipboard as ClipboardIcon,
+  ArrowRight,
+  Flashlight,
+} from "lucide-react";
 import { CardGameMode } from "~/ui/CardGameMode.ui";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
@@ -14,71 +21,83 @@ export default function GameMode() {
   const handleGameMode = (gameModeId: string) => {
     return () => {
       setGameModeSelected(gameModeId);
-    }
-  }
-  
+    };
+  };
+
   const cancelGameMode = () => {
     setGameModeSelected("");
-  }
-  
+  };
+
   const gameModes = [
     {
       id: "vs-computer",
       title: "Solo",
-      description: "Desafía al Bot y demuestra tu dominio de la programación funcional",
+      description:
+        "Desafía al Bot y demuestra tu dominio de la programación funcional",
       icon: Bot,
+      avaible: true,
       children: (
         <div className="flex flex-row-reverse gap-2">
           <Button
-            className="w-28 bg-transparent text-white border-primary bg-black/50 hover:bg-black/30"
+            className="w-28 text-white border-primary bg-black/50 hover:bg-black/30"
             onClick={() => navigate(`/play/vs-computer/${difficulty.advanced}`)}
           >
             Avanzado
           </Button>
           <Button
-            className="w-28 bg-transparent text-white border-primary bg-black/50 hover:bg-black/30"
+            className="w-28 text-white border-primary bg-black/50 hover:bg-black/30"
             onClick={() => navigate(`/play/vs-computer/${difficulty.normal}`)}
           >
             Normal
           </Button>
           <Button
-            className="w-28 bg-transparent text-white border-primary bg-black/50 hover:bg-black/30"
+            className="w-28 text-white border-primary bg-black/50 hover:bg-black/30"
             onClick={() => navigate(`/play/vs-computer/${difficulty.easy}`)}
           >
             Facil
           </Button>
         </div>
-      )
+      ),
     },
     {
       id: "invite",
       title: "Invitar jugador",
       description: "Reta a un amigo a jugar mediante un enlace de invitación",
       icon: Mail,
+      avaible: false,
       children: (
         <div className="flex flex-col gap-2">
           <p className="text-white text-center">Código de invitación</p>
-            <div className="flex flex-row text-center py-2 px-10 bg-black/50 rounded-lg">
-              <p className="text-white">21K2-1A3J</p>
-              <button className="relative hover:text-white/70" onClick={() => navigator.clipboard.writeText("21K2-1A3J")}>
-                <span><ClipboardIcon className="absolute bottom-1 top-1 left-4 h-4 w-4"/></span>
-              </button>
-            </div>
-            <Link
-              to="/play/invite"
-              className="text-center flex flex-row items-center justify-center gap-2 rounded-lg py-2 px-4 bg-black/50 hover:bg-black/30"
+          <div className="flex flex-row text-center py-2 px-10 bg-black/50 rounded-lg">
+            <p className="text-white">21K2-1A3J</p>
+            <button
+              className="relative hover:text-white/70"
+              onClick={() => navigator.clipboard.writeText("21K2-1A3J")}
             >
-              <p className="text-white text-[16px] text-font-bold">Entrar a la Sala</p>
-              <ArrowRight className="w-4 h-4" />
-            </Link>
+              <span>
+                <ClipboardIcon className="absolute bottom-1 top-1 left-4 h-4 w-4" />
+              </span>
+            </button>
+          </div>
+          <Link
+            to="/play/invite"
+            className="text-center flex flex-row items-center justify-center gap-2 rounded-lg py-2 px-4 bg-black/50 hover:bg-black/30"
+          >
+            <p className="text-white text-[16px] text-font-bold">
+              Entrar a la Sala
+            </p>
+            <ArrowRight className="w-4 h-4" />
+          </Link>
         </div>
-      )
+      ),
     },
     {
       id: "join",
       title: "Unirse a partida",
-      description: "Accede a una partida existente usando un código de invitación compartido",
+      description:
+        "Accede a una partida existente usando un código de invitación compartido",
       icon: DoorOpen,
+      avaible: false,
       children: (
         <Input
           placeholder="Código de invitación"
@@ -90,45 +109,48 @@ export default function GameMode() {
             }
           }}
         />
-      )
+      ),
     },
-  ]
+  ];
 
   return (
-  <>
-  <Navbar />
-  <div className="min-h-screen bg-black flex flex-col">
-    <main className="flex-1 pt-24 px-4">
-      <div className="container mx-auto max-w-6xl">
-        {/* Header */}
-        <div className="mb-16 text-center">
-          <h1 className="text-4xl md:text-5xl font-bold text-white mb-4 text-balance">Elije tu modo de juego</h1>
-          <p className="text-white/70 text-lg max-w-2xl mx-auto">
-            Selecciona cómo deseas jugar y comienza tu aventura en CardComposer
-          </p>
-        </div>
+    <>
+      <Navbar />
+      <div className="min-h-screen bg-black flex flex-col">
+        <main className="flex-1 pt-24 px-4">
+          <div className="container mx-auto max-w-6xl">
+            <div className="mb-16 text-center">
+              <h1 className="text-4xl md:text-5xl font-bold text-white mb-4 text-balance">
+                Elije tu modo de juego
+              </h1>
+              <p className="text-white/70 text-lg max-w-2xl mx-auto">
+                Selecciona cómo deseas jugar y comienza tu aventura en
+                CardComposer
+              </p>
+            </div>
 
-          
-        <div className={`${gameModeSelected.length ? "flex flex-col justify-center items-center" : "grid md:grid-cols-3 gap-6 mb-12"}`}>
-          {gameModes.map((mode) => {
-            if (gameModeSelected.length && mode.id !== gameModeSelected) {
-              return null;
-            }
-            return (
-              <CardGameMode
-                mode={mode}
-                gameModeSelected={gameModeSelected}
-                handleGameMode={handleGameMode}
-                cancelGameMode={cancelGameMode}
-                key={mode.id}
-                children={mode.children}
-              />
-          )
-            })}
+            <div
+              className={`${gameModeSelected.length ? "flex flex-col justify-center items-center" : "grid md:grid-cols-3 gap-6 mb-12"}`}
+            >
+              {gameModes.map((mode) => {
+                if (gameModeSelected.length && mode.id !== gameModeSelected) {
+                  return null;
+                }
+                return (
+                  <CardGameMode
+                    mode={mode}
+                    gameModeSelected={gameModeSelected}
+                    handleGameMode={handleGameMode}
+                    cancelGameMode={cancelGameMode}
+                    key={mode.id}
+                    children={mode.children}
+                  />
+                );
+              })}
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
     </>
-  )
+  );
 }
--- app/ui/CardGameMode.ui.tsx
+++ app/ui/CardGameMode.ui.tsx
@@ -1,8 +1,24 @@
-import { Card } from "~/components/ui/card"
-import type { ForwardRefExoticComponent, RefAttributes } from "react"
-import type { LucideProps } from "lucide-react"
-import { X } from "lucide-react"
-import { Button } from "~/components/ui/button"
+import { Card } from "~/components/ui/card";
+import type { ForwardRefExoticComponent, RefAttributes } from "react";
+import type { LucideProps } from "lucide-react";
+import { X } from "lucide-react";
+import { Button } from "~/components/ui/button";
+
+interface GameMode {
+  mode: {
+    id: string;
+    title: string;
+    description: string;
+    icon: ForwardRefExoticComponent<
+      Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
+    >;
+    avaible: boolean;
+  };
+  gameModeSelected: string;
+  handleGameMode: (gameModeId: string) => () => void;
+  cancelGameMode: () => void;
+  children?: React.ReactNode;
+}
 
 export const CardGameMode = ({
   mode,
@@ -10,72 +26,64 @@ export const CardGameMode = ({
   handleGameMode,
   cancelGameMode,
   children,
-}: {
-  mode: {
-    id: string;
-    title: string;
-    description: string;
-    icon: ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>;
-  };
-  gameModeSelected: string;
-  handleGameMode: (gameModeId: string) => () => void;
-  cancelGameMode: () => void;
-  children?: React.ReactNode;
-}) => {
+}: GameMode) => {
   return (
-    <Card
-        className={
-          `p-8 h-full border-2 transition-all duration-300
-          ${gameModeSelected.length ?
-            "border-primary bg-gradient-to-br from-[#FAD126] to-[#FF564E]" :
-            "cursor-pointer hover:border-[#FFD428] hover:bg-white/10 border-white/10 bg-white/5"
-          }`
+    <>
+      <Card
+        className={`p-8 h-full border-2 transition-all duration-300 
+          ${
+            gameModeSelected.length
+              ? "border-primary bg-linear-to-br from-[#FAD126] to-[#FF564E]"
+              : "cursor-pointer hover:border-[#FFD428] hover:bg-white/10 border-white/10 bg-white/5"
+          } 
+          ${mode.avaible ? "" : "pointer-events-none opacity-50"}
+          `}
+        onClick={
+          !!gameModeSelected.length || !mode.avaible
+            ? undefined
+            : handleGameMode(mode.id)
         }
-        onClick={gameModeSelected.length ? undefined : handleGameMode(mode.id)}
         key={mode.id}
-    >
+      >
         <div className="flex flex-col h-full justify-center items-center">
-          {/* Icon */}
           <div
-            className={
-              `mb-6 p-4 rounded-full w-fit transition-all duration-300
-              ${gameModeSelected.length ?
-                "bg-black/50" :
-                "bg-gradient-to-br from-[#FAD126] to-[#FF564E]"
-              }`
-            }
+            className={`mb-6 p-4 rounded-full w-fit transition-all duration-300
+              ${
+                gameModeSelected.length
+                  ? "bg-black/50"
+                  : "bg-linear-to-br from-[#FAD126] to-[#FF564E]"
+              }`}
           >
             <mode.icon className="h-8 w-8 text-white" />
           </div>
 
-          {/* Content */}
-            <h3
-                className="text-2xl font-bold mb-3 text-center text-white"
-            >
-                {mode.title}
-            </h3>
-            <p className={`text-sm mb-6 flex-1 text-center
-                ${gameModeSelected.length ?
-                "text-white" :
-                "text-white/60"
-            }`
-            }>
-                {mode.description}
-            </p>
+          <h3 className="text-2xl font-bold mb-3 text-center text-white">
+            {mode.title}
+          </h3>
+          <p
+            className={`text-sm mb-6 flex-1 text-center
+                ${gameModeSelected.length ? "text-white" : "text-white/60"}`}
+          >
+            {mode.avaible ? mode.description : "En proximas actualizaciones..."}
+          </p>
 
-            {!!gameModeSelected.length &&
+          {!!gameModeSelected.length && (
             <div className="flex flex-col justify-center gap-2">
-                {children}
-                <Button
-                    className="bg-transparent text-white border-primary hover:bg-transparent hover:transform-[translateX(-8px)]"
-                    onClick={cancelGameMode}
-                >
-                    Cancelar
-                    <span><X className="h-4 w-4" /></span>
-                </Button>
+              {children}
+              <Button
+                className="bg-transparent text-white border-primary hover:bg-transparent hover:transform-[translateX(-8px)]"
+                onClick={cancelGameMode}
+              >
+                Cancelar
+                <span>
+                  <X className="h-4 w-4" />
+                </span>
+              </Button>
             </div>
-            }
+          )}
         </div>
-    </Card>
-  )
-}
+      </Card>
+    </>
+  );
+};
+
